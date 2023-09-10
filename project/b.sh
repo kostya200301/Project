8a8cf41f43f929a958088e30cdacf02232e41d8c
kostya200301 <58062706+kostya200301@users.noreply.github.com>
2023-09-10 22:10:26 +0300

    Update b.sh

--- project/b.sh
+++ project/b.sh
@@ -1,6 +1,22 @@
 sudo apt update
+sudo apt-get install pkg-config
+sudo snap install tree
+sudo apt-get install curl zip unzip tar
+sudo apt install make cmake g++
 sudo apt-get install libgflags-dev
 sudo apt-get install nlohmann-json3-dev
+
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg
+./bootstrap-vcpkg.sh
+./vcpkg integrate install
+
+./vcpkg install nlohmann-json
+./vcpkg install gflags
+
+cd ..
+
+
 wget https://download.libsodium.org/libsodium/releases/LATEST.tar.gz
 tar -xzvf LATEST.tar.gz
 cd libsodium-stable
@@ -8,6 +24,11 @@ cd libsodium-stable
 make
 sudo make install
 cd ..
+
+full_path=$(realpath "./vcpkg/installed/x64-linux")
+export CMAKE_PREFIX_PATH="$full_path"
+
+
 mkdir build
 cd build
 cmake ..
